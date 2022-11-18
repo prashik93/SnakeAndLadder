5634f7dd4a42f0113404b7b35d6b4cbe7766bb16
Prashik Kamble <kambleprashik7349@gmail.com>
2022-11-18 21:54:09 +0530

    ADDED : Code to get random number between 1 to 6.

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,6 +1,10 @@
 public class SnakeAndLadder {
+
     static final int START_POSITION = 0;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Problem");
+        double random = Math.floor(Math.random() * 10) % 6 + 1;
+        System.out.println("Result after rolling the die : " + random);
     }
 }
